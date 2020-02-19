8aeef0fadd9dbffd06350159fc2048ad0ca052b6
mgm215 <60325909+mgm215@users.noreply.github.com>
2020-02-19 12:23:36 -0500

    fixing merge issues

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var searchForm = $("#searchForm");
 var searchInput = $(".searchInput");
 var searchSubmit = $("#searchSubmit");
+// var searchSubmit = $("#searchSubmit");
 var searchTerm = '';
 var newsDiv = $("#newsDiv");
 var searchSymbol = '';
@@ -18,13 +19,35 @@ $.ajax({
 
 
 
+    //API cal to alphaavantage
+    // var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=4EOJKMRS4JOT2AEA";
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(response);
+    // });
+
+    // //API call to symbol lookup
+    // var symbolQueryURL = "https://financialmodelingprep.com/api/v3/search?query=microsoft&limit=10";
+    // $.ajax({
+    //     url: symbolQueryURL,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(response);
+    // });
+
+
 function generateStockInfo(){
     console.log(response);
+<<<<<<< Updated upstream
     
 
 
 
 
+=======
+>>>>>>> Stashed changes
 }
 
 // function generateStockNews(){
@@ -32,6 +55,7 @@ function generateStockInfo(){
 
 // }
 
+<<<<<<< Updated upstream
 searchInput.on("submit", function(event){
     event.preventDefault();
     searchTerm = searchInput.val();
@@ -46,6 +70,23 @@ searchInput.on("submit", function(event){
 
     generateStockInfo();
     generateStockNews();
+=======
+searchForm.on("submit", function(event){
+    event.preventDefault();
+    searchTerm = searchInput.val();
+    console.log(searchTerm);
+    getStockQuery();
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(response);
+    //     generateStockInfo(response);
+    // });
+
+    // generateStockInfo();
+    // generateStockNews();
+>>>>>>> Stashed changes
 
 });
 
