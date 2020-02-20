71091c0803a071e278649b0b0742f46634f0a642
mgm215 <60325909+mgm215@users.noreply.github.com>
2020-02-20 11:20:51 -0500

    Added code to display stock info on search and added code to make an NYT API call on search

--- script.js
+++ script.js
@@ -3,71 +3,143 @@ var searchInput = $(".searchInput");
 var searchSubmit = $("#searchSubmit");
 // var searchSubmit = $("#searchSubmit");
 var searchTerm = '';
-var newsDiv = $("#newsDiv");
+var stockInfo = $("#stockInfo");
 var searchSymbol = '';
-
-var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + searchSymbol + "&interval=5min&apikey=4EOJKMRS4JOT2AEA";
-   
-
-// $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function(response) {
-//         console.log(response);
-//         generateStockInfo(response);
-//     });
+var searchTerm = '';
+var startYear = null;
+var endYear = null;
+queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=4EOJKMRS4JOT2AEA";
 
 
 
-    //API cal to alphaavantage
-    // var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=4EOJKMRS4JOT2AEA";
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     console.log(response);
-    // });
-
-    // //API call to symbol lookup
-    // var symbolQueryURL = "https://financialmodelingprep.com/api/v3/search?query=microsoft&limit=10";
-    // $.ajax({
-    //     url: symbolQueryURL,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     console.log(response);
-    // });
 
 
-function generateStockInfo(response){
-    // placeholderDiv.text(response.name);
-    // placeholderDiv.text(response.last);
-    // placeholderDiv.text(response.high);
-    // placeholderDiv.text(response.low);
-    // placeholderDiv.text(response.volume);
+var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=compact&apikey=4EOJKMRS4JOT2AEA";
 
-    
+
+function getInfo(){
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    // return(response);
+    console.log(response);
+    var newStockName = $("<p>");
+    var newStockPrice = $("<p>");
+    var newStockHigh = $("<p>");
+    var newStockLow = $("<p>");
+    var newStockVol = $("<p>");
+    newStockName.text(response["Meta Data"]["2. Symbol"]);
+    stockInfo.append(newStockName);
+    // var lastRefreshed = response["Meta Data"]["Last Refreshed"];
+    // console.log(lastRefreshed);
+    var firstKey = response["Time Series (Daily)"][Object.keys(response["Time Series (Daily)"])[0]];
+    console.log(firstKey);
+    newStockPrice.text(firstKey["1. open"]);
+    stockInfo.append(newStockPrice);
+    newStockHigh.text(firstKey["2. high"]);
+    stockInfo.append(newStockHigh);
+    newStockLow.text(firstKey["3. low"]);
+    stockInfo.append(newStockLow);
+    newStockVol.text(firstKey["5. volume"]);
+    stockInfo.append(newStockVol);
+});
+
 }
 
-// function generateStockNews(){
 
 
-// }
+
 
 searchForm.on("submit", function(event){
     event.preventDefault();
     searchTerm = searchInput.val();
-    console.log(searchTerm);
-    // getStockQuery();
+    var symbolQueryURL = "https://financialmodelingprep.com/api/v3/search?query=" + searchTerm + "&limit=10";
+    $.ajax({
+        url: symbolQueryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+
+    newsQueryURL = getNewsQuery();
+    search();
+    
+    // console.log(searchSymbol);
+    // queryURL = getQuery();
+
+    // var stockInfo = getInfo();
+    // console.log(stockInfo);
     // $.ajax({
     //     url: queryURL,
     //     method: "GET"
     // }).then(function(response) {
     //     console.log(response);
-    //     generateStockInfo(response);
+        
     // });
-
-    // generateStockInfo();
-    // generateStockNews();
-
+ 
 });
 
+
+
+// Add Script for portfolio page
+
+
+
+
+// NYT News Search Code
+
+
+
+
+
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=gtGyME9eJStqbpLqVNHQQKExu01uGU0X";
+
+
+function getNewsQuery() {
+    if (startYear !== null && endYear !== null) {
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startYear + "0101&end_date=" + endYear + "1231&q=" + searchTerm + "&api-key=gtGyME9eJStqbpLqVNHQQKExu01uGU0X";
+    
+    }
+
+    else if (startYear === null && endYear !== null) {
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=19000101&end_date=" + endYear + "1231&q=" + searchTerm + "&api-key=gtGyME9eJStqbpLqVNHQQKExu01uGU0X";
+    }
+    else if(startYear !== null && endYear === null) {
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startYear + "0101&end_date=20201231&q=" + searchTerm + "&api-key=gtGyME9eJStqbpLqVNHQQKExu01uGU0X";
+    }
+    else {
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=gtGyME9eJStqbpLqVNHQQKExu01uGU0X";
+
+    }
+    return queryURL;
+}
+
+function search(){
+    $.ajax({
+        url: newsQueryURL,
+        method : "GET"
+    }).then(function(response){
+        console.log(response)
+        // for(i = 0; i < articleNum; i++) {
+        //     let div = document.createElement("div");
+        //     let headline = document.createElement("p");
+        //     headline.textContent = response.response.docs[i].headline.main;
+        //     let datePub = document.createElement("p");
+        //     let pubDate = response.response.docs[i].pub_date;
+        //     datePub.textContent = "Released: " + pubDate.substring(0, 9);
+        //     let author = document.createElement("p");
+        //     let name = response.response.docs[i].byline.original;
+        //     author.textContent = "Written: " + name;
+        //     let snippet = document.createElement("p");
+        //     let snipText = response.response.docs[i].lead_paragraph;
+        //     snippet.textContent = "Snippet: " + snipText;
+            
+            // div.appendChild(headline);
+            // div.appendChild(datePub);
+            // div.appendChild(author);
+            // div.appendChild(snippet);
+            // displayArticles.appendChild(div);
+        })
+}
